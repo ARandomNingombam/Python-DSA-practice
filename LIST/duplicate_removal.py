555418bd452cc37b961d07071d9ab1ecd21fecc5
ARandomNingombam <ningombamarvind@gmail.com>
2026-07-30 13:23:46 +0530

    Duplicate_removal changed

--- LIST/duplicate_removal.py
+++ LIST/duplicate_removal.py
@@ -1,10 +1,9 @@
 
 ar = list(map(int,input("Enter the numbers: ").split()))
+rev_dupli = []
 
-rev=[]
-n = len(ar)
+for i in range(len(ar)):
+    if ar[i] not in rev_dupli:
+        rev_dupli.append(ar[i])
 
-for i in range(n):
-    rev.append(ar[n-i-1])
-
-print(f"The reversed list is ", rev)
+print(f"Duplicates Removed!: ", rev_dupli)
